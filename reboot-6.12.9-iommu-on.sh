e92c363744a601a2d14cb9b44e32d65d1aadfec8
Siyuan <siyuanc3@illinois.edu>
2025-02-12 20:56:21 -0500

    reboot utils

---
+++ reboot-6.12.9-iommu-on.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+kernel="6.12.9"
+iommu="on"
+
+grub_src="grub_linux/grub_linux_${kernel}_${iommu}"
+if [ -f $grub_src ]; then
+#   echo "File does not exist."
+    sudo cp $grub_src /etc/default/grub
+else
+    echo "$grub_src doesn't exist"
+    exit 1
+fi
+
+
+sudo update-grub2
+
+echo "Reboot to ${kernel} IOMMU=${iommu} in 10 seconds."
+
+sudo reboot
