b9688e5a525646124551517a87f2ac978a08d49f
OwenCochell <owencochell@gmail.com>
2026-02-24 15:31:32 -0600

    Now invokes memory collection script in background

--- scripts/sosp24-experiments/vm_flows_exp.sh
+++ scripts/sosp24-experiments/vm_flows_exp.sh
@@ -126,11 +126,14 @@ fi
 iommu_config="host-${host_iommu_config}-guest-${guest_iommu_config}-$virt_tech"
 echo "iommu_config: $iommu_config"
 
-
 client_cores="0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31"
 server_cores="0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31"
 
 timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+
+echo "Starting memory collection script..."
+sudo bash collect-mem-stats.sh "../utils/reports/${timestamp}-mem-stats-${iommu_config}.csv" &
+
 N_RUNS=3
 for socket_buf in 1; do
     for ring_buffer in 512; do
