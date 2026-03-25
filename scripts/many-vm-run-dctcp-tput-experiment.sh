60ad26d8e568b4bd17608796364cb7431e0e02bb
Leshna Balara <leshnabalara2001@gmail.com>
2026-03-25 10:26:44 -0500

    path 2

--- scripts/many-vm-run-dctcp-tput-experiment.sh
+++ scripts/many-vm-run-dctcp-tput-experiment.sh
@@ -555,7 +555,7 @@ log_info "############################################################"
 cleanup
 post_exp_cleanup
 
-cd "$SCRIPT_DIR/.." || exit 1
+cd "$SCRIPT_DIR" || exit 1
 
 if [ "$MLC_CORES" != "none" ]; then
 	log_info "MLC cores were used."
