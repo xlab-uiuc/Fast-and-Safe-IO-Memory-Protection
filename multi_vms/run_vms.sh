7d749cc1e5749d3276eef149a71d0affbc4692f2
Leshna Balara <leshnabalara2001@gmail.com>
2026-03-30 14:27:03 -0500

    server_loader

--- multi_vms/run_vms.sh
+++ multi_vms/run_vms.sh
@@ -15,7 +15,7 @@ SKIP_RESET_HOST=0
 
 # --- Hardcoded experiment config ---
 GIT_REPO="/home/schai/viommu"
-GIT_BRANCH="many-vm-setup"
+GIT_BRANCH="many-vm-setup-siyuan"
 VM_SCRIPT="cd /home/schai/viommu/scripts/sosp24-experiments; ./many_vm_flows_exp.sh"
 
 # --- Host paths (this script runs ON the host) ---
